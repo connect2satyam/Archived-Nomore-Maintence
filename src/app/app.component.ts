5d85427051fe2921cb7c245feb56879f0ca54da4
Satyanarayana Devi <connect2satyam@gmail.com>
2020-04-26 19:39:57 +0530

    Title updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'satyad-remote-dev';
-
   toggle() {
     $('#sidebar').toggleClass('active');
   }
