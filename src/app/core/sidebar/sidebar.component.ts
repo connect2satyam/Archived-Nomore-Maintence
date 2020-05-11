43e2b80bffe33197076b4d8a20b08684d7717c7c
Satyanarayana Devi <connect2satyam@gmail.com>
2020-05-12 00:21:14 +0530

    Sidebar validations added.

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,10 +1,21 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'satyas-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-  constructor() {}
+export class SidebarComponent implements OnInit {
+  isUserLogged = false;
+  userName: string;
+  subOnlineStatus: Subscription;
+
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    this.subOnlineStatus = this.authService.getOnlineStatusAction$.subscribe(isUserLogged => {
+      this.isUserLogged = isUserLogged;
+    });
+  }
 }
