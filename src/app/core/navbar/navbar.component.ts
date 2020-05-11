4da7b890e5fb21c6e810ef2018d0c0952e5617d0
Satyanarayana Devi <connect2satyam@gmail.com>
2020-05-11 23:10:13 +0530

    Some UI Changes.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'satyas-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   constructor() {}
