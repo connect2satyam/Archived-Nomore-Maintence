75e2c1eec54b47dbd18242136785dc79a304e64f
Satyanarayana Devi <connect2satyam@gmail.com>
2020-05-23 16:34:03 +0530

    circle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import $ from 'jquery';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 
 @Component({
@@ -7,8 +9,56 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('circle', { static: false }) myCircle: ElementRef;
+
+  styleClass = {
+    top: '1px',
+    left: '1px'
+  };
+
+  constructor(private renderer: Renderer2) { }
+
+  getStyles() {
+    return this.styleClass;
+  }
+  
+  ngOnInit(): void {
+
+  }
   toggle() {
     $('#mySidebar').toggleClass('active');
+  }
+
+  ngAfterViewInit(): void {
+    // this.someInput.nativeElement.value = 'Anchovies! 🍕🍕';
+
+    // this.renderer.setStyle(this.someInput.nativeElement, 'background-color', '#ff0000');
+
+    // this.renderer.setStyle(this.myCircle.nativeElement, 'background-color', '#ff9999');
+    // this.renderer.setStyle(this.myCircle.nativeElement, 'backgroundColor', 'grey');
+    // this.renderer.setStyle(this.myCircle.nativeElement, 'color', 'red');
+    // this.renderer.setStyle(this.myCircle.nativeElement, 'top', 179);
+    // this.myCircle.nativeElement.value = 'I Love You';
+    // this.renderer.setAttribute(this.myCircle.nativeElement, 'outerText', 'I Love You');
+
+    const source = fromEvent(document, 'mousemove').pipe(map((e: MouseEvent) => {
+      return {
+        x: e.clientX,
+        y: e.clientY
+      };
+    }));
+
+    source.subscribe(
+      value => {
+        // this.styleClass.left = value.x - 10 + 'px';
+        // this.styleClass.top = value.y - 84 + 'px';
+        this.styleClass.left = value.x - 1 + 'px';
+        this.styleClass.top = value.y - 74 + 'px';
+        console.log(value.x);
+      },
+      e => console.log(`error ${e}`),
+      () => console.log(`complete`)
+    );
   }
 }
--- src/app/core/welcome/welcome.component.ts
+++ src/app/core/welcome/welcome.component.ts
@@ -1,64 +1,11 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'satyas-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit, AfterViewInit {
+export class WelcomeComponent {
   public pageTitle = 'Welcome to my demo';
-  styleClass = {
-    top: '100px',
-    left: '100px'
-  };
-  // circleObj = document.getElementById('saCircle');
-  @ViewChild('saCircle', { static: false }) myCircle: ElementRef;
-  @ViewChild('someInput') someInput: ElementRef;
-  constructor(private renderer: Renderer2) { }
-
-  getStyles() {
-    return this.styleClass;
-  }
-
-
-  ngAfterViewInit(): void {
-    // this.someInput.nativeElement.value = 'Anchovies! 🍕🍕';
-
-    // this.renderer.setStyle(this.someInput.nativeElement, 'background-color', '#ff0000');
-
-    // this.renderer.setStyle(this.myCircle.nativeElement, 'background-color', '#ff9999');
-    // this.renderer.setStyle(this.myCircle.nativeElement, 'backgroundColor', 'grey');
-    // this.renderer.setStyle(this.myCircle.nativeElement, 'color', 'red');
-    // this.renderer.setStyle(this.myCircle.nativeElement, 'top', 179);
-    // this.myCircle.nativeElement.value = 'I Love You';
-    // this.renderer.setAttribute(this.myCircle.nativeElement, 'outerText', 'I Love You');
-
-    const source = fromEvent(document, 'mousemove').pipe(map((e: MouseEvent) => {
-      return {
-        x: e.clientX,
-        y: e.clientY
-      };
-    }));
-
-    source.subscribe(
-      value => {
-        // this.styleClass.left = value.x - 10 + 'px';
-        // this.styleClass.top = value.y - 84 + 'px';
-        this.styleClass.left = value.x - 1 + 'px';
-        this.styleClass.top = value.y - 74 + 'px';
-        console.log(value.x);
-      },
-      e => console.log(`error ${e}`),
-      () => console.log(`complete`)
-    );
-  }
-
-  ngOnInit(): void {
-    // this.styleClass.top = '600px';
-
-
-  }
 }
 
