e3b85a54f8b08e66cbcff65b12b3cf60c862be04
Satyanarayana Devi <connect2satyam@gmail.com>
2020-06-12 09:55:05 +0530

    Some practice

--- src/app/concepts/content-projection/host/host.component.ts
+++ src/app/concepts/content-projection/host/host.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { DateviewerComponent } from '../dateviewer/dateviewer.component';
+import { reduce } from 'rxjs/operators';
 
 @Component({
   selector: 'satyas-host',
@@ -9,11 +10,16 @@ import { DateviewerComponent } from '../dateviewer/dateviewer.component';
 export class HostComponent implements OnInit, AfterViewInit {
 
   @ViewChild(DateviewerComponent) dateviewerComponent: DateviewerComponent;
+
+  list: number[] = [1, 2, 3];
+  list1: Array<number> = [4, 5, 6];
+  tuple: [string, number, boolean?];
   constructor() {
     // console.log(this.dateviewerComponent?.today);
   }
 
   ngAfterViewInit(): void {
+
     // console.log(this.dateviewerComponent.today);
 
     // setInterval(() => {
@@ -22,6 +28,21 @@ export class HostComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+    const color =  Color.Blue;
+
+    // this.tuple = ['satya', 300000, false];
+    // console.log(this.tuple);
+    // this.list.forEach((x) => {
+    //   console.log(x);
+    // });
+
+    // console.log(this.list1);
   }
 
+}
+
+enum Color {
+  Red,
+  Green  = 1000,
+  Blue
 }
